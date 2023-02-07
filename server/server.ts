3986a8b4381b4767cc558d972fcc889741be71b5
sakilk130 <sakilk130@gmail.com>
2023-02-08 03:55:23 +0600

    feat: (server): db config

--- server/server.ts
+++ server/server.ts
@@ -1,10 +1,15 @@
 import cors from 'cors';
+import dotenv from 'dotenv';
 import express from 'express';
 import routesV1 from './api/v1/routes';
+import connectDB from './config/db';
 
+dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 8000;
 
+connectDB();
+app.use(express.json());
 app.use(cors());
 app.use('/api/v1', routesV1);
 
