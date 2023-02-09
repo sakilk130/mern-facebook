20047ac14c7888c0356a24ea0d9f7c30d6df4a5f
sakilk130 <sakilk130@gmail.com>
2023-02-09 18:57:16 +0600

    feat: (client): login page form validation

--- client/src/pages/login/index.tsx
+++ client/src/pages/login/index.tsx
@@ -1,8 +1,18 @@
 import { Link } from 'react-router-dom';
 import PageTitle from '../../components/page-title';
 import styles from './styles/login.module.css';
+import { Form, FormikProvider, useFormik, Field, ErrorMessage } from 'formik';
+import { initialValues, validationSchema } from './formik/formik';
 
 const Login = () => {
+  const formik = useFormik({
+    initialValues,
+    validationSchema,
+    onSubmit: (values) => {
+      console.log(values);
+    },
+  });
+
   return (
     <>
       <PageTitle title="Login | Facebook" />
@@ -17,20 +27,49 @@ const Login = () => {
             Facebook helps you connect and share with the people in your life.
           </h1>
         </div>
+
         <div className={styles.right}>
-          <div className={styles.card}>
-            <div className={styles.input}>
-              <input type="text" placeholder="Email address or phone number" />
-            </div>
-            <div className={styles.input}>
-              <input type="password" placeholder="Password" />
-            </div>
-            <button className={styles.loginBtn}>Log In</button>
-            <div className={styles.forgotBtn}>
-              <Link to="/forgotten-password">Forgotten password?</Link>
-            </div>
-            <button className={styles.newAccountBtn}>Create New Account</button>
-          </div>
+          <FormikProvider value={formik}>
+            <Form>
+              <div className={styles.card}>
+                <div
+                  className={`${styles.input} ${
+                    formik.errors.email && styles.redBorder
+                  }`}
+                >
+                  <Field
+                    type="email"
+                    placeholder="Email address"
+                    name="email"
+                  />
+                  <div className={styles.fieldError}>
+                    <ErrorMessage name="email" />
+                  </div>
+                </div>
+                <div
+                  className={`${styles.input} ${
+                    formik.errors.password && styles.redBorder
+                  }`}
+                >
+                  <Field
+                    type="password"
+                    placeholder="Password"
+                    name="password"
+                  />
+                  <div className={styles.fieldError}>
+                    <ErrorMessage name="password" />
+                  </div>
+                </div>
+                <button className={styles.loginBtn}>Log In</button>
+                <div className={styles.forgotBtn}>
+                  <Link to="/forgotten-password">Forgotten password?</Link>
+                </div>
+                <button className={styles.newAccountBtn}>
+                  Create New Account
+                </button>
+              </div>
+            </Form>
+          </FormikProvider>
           <p className={styles.instruction}>
             Create a Page for a celebrity, brand or business.
           </p>
