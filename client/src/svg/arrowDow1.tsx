9dabfa7a66166e9aeae6fbf30d35605d356db8ab
sakilk130 <sakilk130@gmail.com>
2023-02-09 14:44:39 +0600

    feat: (client): starter

---
+++ client/src/svg/arrowDow1.tsx
@@ -0,0 +1,13 @@
+const ArrowDown1 = () => {
+  return (
+    <svg width="20" height="20" viewBox="0 0 16 16">
+      <path
+        fillRule="nonzero"
+        d="M452.707 549.293a1 1 0 00-1.414 1.414l4 4a1 1 0 001.414 0l4-4a1 1 0 00-1.414-1.414L456 552.586l-3.293-3.293z"
+        transform="translate(-448 -544)"
+      />
+    </svg>
+  );
+};
+
+export default ArrowDown1;
