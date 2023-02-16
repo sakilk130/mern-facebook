bca3f7b3cfd2f8a7217ed42464624ac1364ccf40
sakilk130 <sakilk130@gmail.com>
2023-02-16 21:05:29 +0600

    feat: (client): create post

--- client/src/pages/home/index.tsx
+++ client/src/pages/home/index.tsx
@@ -1,7 +1,11 @@
 import cls from 'classnames';
 import Header from '../../components/header';
-import { HomeLeft, HomeRight } from '../../components/home';
-import Stories from '../../components/home/components/stories';
+import {
+  CreatePost,
+  HomeLeft,
+  HomeRight,
+  Stories,
+} from '../../components/home';
 import styles from './styles/home.module.css';
 
 const Home = () => {
@@ -11,6 +15,7 @@ const Home = () => {
       <HomeLeft />
       <div className={cls(styles.middle)}>
         <Stories />
+        <CreatePost />
       </div>
       <HomeRight />
     </div>
