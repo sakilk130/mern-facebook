504731b238ecb66a4171e69542f1e9a041cdb440
sakilk130 <sakilk130@gmail.com>
2023-02-17 23:12:59 +0600

    fix: (client): responsive home

--- client/src/components/home/components/stories/index.tsx
+++ client/src/components/home/components/stories/index.tsx
@@ -1,10 +1,27 @@
 import cls from 'classnames';
+import { useMediaQuery } from 'react-responsive';
 import { stories } from '../../../../data/home';
 import { ArrowRight, Plus } from '../../../../svg';
 import Story from './components/story';
 import styles from './styles/stories.module.css';
 
 const Stories = () => {
+  const query1175px = useMediaQuery({
+    query: '(max-width: 1175px)',
+  });
+  const query900px = useMediaQuery({
+    query: '(max-width: 900px)',
+  });
+
+  let max = stories.length;
+  if (query900px) {
+    max = 5;
+  } else if (query1175px) {
+    max = 4;
+  } else {
+    max = stories.length;
+  }
+
   return (
     <div className={cls(styles.container)}>
       <div className={cls(styles.story)}>
@@ -16,7 +33,7 @@ const Stories = () => {
           <div className={cls(styles.createStory)}>Create Story</div>
         </div>
       </div>
-      {stories.map((story) => (
+      {stories.slice(0, max).map((story) => (
         <Story
           key={story.profile_name}
           image={story.image}
