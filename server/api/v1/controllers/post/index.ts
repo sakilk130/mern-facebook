b7791bc3c20b81d53c603c770a24ea58f8617396
sakilk130 <sakilk130@gmail.com>
2023-02-25 15:25:08 +0600

    feat: (server): populate user data with post

--- server/api/v1/controllers/post/index.ts
+++ server/api/v1/controllers/post/index.ts
@@ -19,7 +19,10 @@ export const createPost = async (req: Request, res: Response) => {
 
 export const getAllPosts = async (_: Request, res: Response) => {
   try {
-    const posts = await Post.find().lean();
+    const posts = await Post.find()
+      .populate('user', 'firstName lastName gender userName picture')
+      .sort({ createdAt: -1 })
+      .lean();
     return res.status(200).json({
       success: true,
       data: posts,
